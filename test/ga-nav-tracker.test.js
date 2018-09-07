578ad85eea32115a08c6e7cd1126717b0cc93ce3
Alejandro Pérez Martín <aleperez92@gmail.com>
2018-09-07 01:39:02 +0100

    Update tests and example

--- test/ga-nav-tracker.test.js
+++ test/ga-nav-tracker.test.js
@@ -1,39 +1,100 @@
+/* eslint-disable new-cap */
 import GANavTracking from '../index';
 import test from 'tape-rollup';
-import { findOne } from 'domassist';
+
+// GA Stub
+let gaData = [];
+window._gaq = { // eslint-disable-line no-underscore-dangle
+  data: null,
+  clear: assert => {
+    gaData = [];
+  },
+  push(arr) {
+    if (Array.isArray(arr)) {
+      gaData.push(arr); // eslint-disable-line no-underscore-dangle
+    }
+  }
+};
+
+window.ga = function (...args) {
+  gaData = args;
+};
 
 const init = () => {
   const container = document.createElement('div');
   container.id = 'fixture';
+  document.body.appendChild(container);
+};
+
+const setup = () => {
+  const container = document.getElementById('fixture');
   container.innerHTML = `
     <nav>
-    <ul data-nav-tracker="header">
-      <li><a href="#">Link 1</a></li>
-      <li data-nav-tracker-dropdown="Dropdown">
-        <ul>
-          <li><a href="#">Link 1</a></li>
-        </ul>
-      </li>
-    </ul>
-  </nav>
+      <ul data-nav-tracker="header">
+        <li><a id="link1" href="#">Link 1</a></li>
+        <li data-nav-tracker-dropdown="Dropdown">
+          <ul>
+            <li><a id="link2" href="#">Link 2</a></li>
+          </ul>
+        </li>
+        <li data-nav-tracker="footer">
+          <a id="link3" href="#">
+            <div id="text">Link 3</div>
+          </a>
+        </li>
+      </ul>
+    </nav>
   `;
-  document.body.appendChild(container);
+
+  window._gaq.clear(); // eslint-disable-line no-underscore-dangle
 };
 
 init();
 
-test('init - Parameters', assert => {
-  let output = false;
+test('GANavTracking plugin exists', assert => {
+  assert.equal(typeof GANavTracking, 'object', 'object is defined');
+  assert.equal(typeof GANavTracking.init, 'function', 'init() is defined');
+  assert.end();
+});
 
-  GANavTracking.init(() => {
-    assert.ok(true, 'Callback is being called');
-    assert.equal(output, false, 'Callback timeout works');
-    assert.end();
-  }, 3000);
+test('Track an element on click', assert => {
+  setup();
+  const el = document.getElementById('link1');
 
-  findOne('a').click();
+  el.click();
 
-  setTimeout(() => {
-    output = true;
-  }, 1000);
+  assert.equal(gaData.length, 1, 'one event tracked');
+  assert.equal(gaData[0][0], '_trackEvent', 'tracks event');
+  assert.equal(gaData[0][1], 'nav-tracker', 'category matches');
+  assert.equal(gaData[0][2], 'Link 1', 'action matches');
+  assert.equal(gaData[0][3], 'header', 'label matches');
+  assert.end();
+});
+
+test('Track an element on click within dropdown', assert => {
+  setup();
+  const el = document.getElementById('link2');
+
+  el.click();
+
+  assert.equal(gaData.length, 1, 'one event tracked');
+  assert.equal(gaData[0][0], '_trackEvent', 'tracks event');
+  assert.equal(gaData[0][1], 'nav-tracker', 'category matches');
+  assert.equal(gaData[0][2], 'Dropdown - Link 2', 'action matches');
+  assert.equal(gaData[0][3], 'header', 'label matches');
+  assert.end();
+});
+
+test('Track click on element inside the anchor', assert => {
+  setup();
+  const el = document.getElementById('text');
+
+  el.click();
+
+  assert.equal(gaData.length, 1, 'one event tracked');
+  assert.equal(gaData[0][0], '_trackEvent', 'tracks event');
+  assert.equal(gaData[0][1], 'nav-tracker', 'category matches');
+  assert.equal(gaData[0][2], 'Link 3', 'action matches');
+  assert.equal(gaData[0][3], 'footer', 'label matches');
+  assert.end();
 });
